ccf6071c36e6d41d937c7f8e9b9b901e4c16651c
Amit Singh <amitsingh100014@gmail.com>
2026-01-13 22:44:07 +0530

    Add variables and console usage example in JavaScript

---
+++ 01_variables.js
@@ -0,0 +1,19 @@
+const accountId =123456
+let accountEmail ="amit@gmail.com"
+var accountPassward ="3434343"
+accountCity ="lucknow"
+let accountState;
+
+accountEmail="abcd@gmailcom"
+accountPassward="56565654"
+accountCity="kanpur"
+
+
+console.log(accountId)
+/*
+Prefer not to use var
+becaause of issue in block scope and functional scope
+
+*/
+
+console.table([accountEmail,accountId,accountPassward,accountState,accountCity])
