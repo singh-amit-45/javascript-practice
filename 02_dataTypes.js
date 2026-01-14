907e6648ed634d84a04fe3e182fe87101665c325
Amit Singh <amitsingh100014@gmail.com>
2026-01-14 12:24:53 +0530

    Add examples of JavaScript data types and typeof usage

---
+++ 02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" //  treat all js code as newer version
+
+//  alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3) // code readability should be high
+
+    console.log("amit")
+
+    let name ="amit"
+    let age =21
+    let isLogedIn = false
+
+    // number => 2 to power 53
+    // bigint 
+
+    // string=>""
+    // bollean =>true/false
+    // null => standalone value
+    // undefined => value is not assigned
+    // symbol => unique
+    
+    
+    // object 
+
+    console.log(typeof null); // object
+    console.log(typeof undefined);// undefinded
