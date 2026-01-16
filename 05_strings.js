bc1ce8c3ddfc6f1a9d5f5a486cc474d7a32fe721
Amit Singh <amitsingh100014@gmail.com>
2026-01-16 15:02:51 +0530

    Add JavaScript string methods examples and template literals usage

---
+++ 05_strings.js
@@ -0,0 +1,35 @@
+const name = "amit"
+const repoCount = 11
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('singh-amit-11')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-4, 2)
+console.log(anotherString);
+
+const newStringOne = "   amit    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://amit.com/amit%20singh"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
