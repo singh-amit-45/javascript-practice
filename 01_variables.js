358585c9d42b42ba2ccd9f36eb81fe098048e854
Amit Singh <amitsingh100014@gmail.com>
2026-01-19 23:06:31 +0530

    Added all JS practice files with folder structure

---
+++ 01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "amit@google.com"
+var accountPassword = "12345"
+accountCity = "lucknow"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "amai.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
